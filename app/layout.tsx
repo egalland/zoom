4d1d715fcf0c3010f7c1b5601f60061c95490e26
img_5528.png <img5528@gmail.com>
2024-04-05 23:22:22 +0200

    home grid

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,13 +11,31 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-dark-2`}>{children}</body>
+      <ClerkProvider
+        appearance={{
+          layout: {
+            logoImageUrl: '/icons/zoom-logo.svg',
+            socialButtonsVariant: 'iconButton',
+          },
+          variables: {
+            colorText: '#FFF',
+            colorPrimary: '#0E78F9',
+            colorBackground: '#1C1f2E',
+            colorInputBackground: '#252A41',
+            colorInputText: '#FFF',
+          }
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -35,7 +35,6 @@ const Sidebar = () => {
                 )
             })}
         </div>
-        Sidebar
     </section>
   )
 }
