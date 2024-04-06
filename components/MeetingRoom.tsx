0ffeecf1d6097ee785f4dc97b94b6ebcb2188480
img_5528.png <img5528@gmail.com>
2024-04-06 22:40:29 +0200

    end call button

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { cn } from '@/lib/utils';
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import { LayoutList, Users } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import EndCallButton from './EndCallButton';
+  
+
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
+
+const MeetingRoom = () => {
+
+    const searchParams = useSearchParams();
+    const isPersonalRoom = !!searchParams.get('personal');
+    const router = useRouter();
+    const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
+    const [showParticipants, setShowParticipants] = useState(false);
+
+    const CallLayout = () => {
+        switch (layout) {
+            case 'grid':
+                return <PaginatedGridLayout />
+                break;
+            case 'speaker-right':
+                return <SpeakerLayout participantsBarPosition='left' />
+                break;
+            default:
+                return <SpeakerLayout participantsBarPosition='right' />
+                break;
+        }
+    };
+
+  return (
+    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+        <div className='relative flex size-full item-center justify-center'>
+            <div className='flex size-full max-w-[1000px] items-center'>
+                <CallLayout />
+            </div>
+            <div className={cn('h-[calc(100vh-86px)] hidden ml-2', { 'show-block': showParticipants })}>
+                <CallParticipantsList
+                    onClose={() => setShowParticipants(false) }
+                />
+            </div>
+            <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
+                <CallControls />
+
+                <DropdownMenu>
+                    <div className='flex items-center'>
+                        <DropdownMenuTrigger
+                            className='cursor-pointer rounded-2xl bg-[#19232D] px-4 py-2 hover:bg-[#4C535B]'
+                        >
+                            <LayoutList size={20} className='text-white' />
+                        </DropdownMenuTrigger>
+                    </div>
+                    <DropdownMenuContent
+                        className='border-dark-1 bg-dark-1 text-white'
+                    >
+                        {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+                            <div key={index}>
+                                <DropdownMenuItem
+                                    className='cursor-pointer'
+                                    onClick={() => {
+                                        setLayout(item.toLowerCase() as CallLayoutType);
+                                    }}
+                                >
+                                    { item }
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator className='border-dark-1' />
+                            </div>
+                        ))}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <CallStatsButton />
+                <button onClick={() => setShowParticipants(prev => !prev)}>
+                    <div className='cursor-pointer rounded-2xl bg-[#19232D] px-4 py-2 hover:bg-[#4C535B]'>
+                        <Users size={20} className='text-white' />
+                    </div>
+                </button>
+                {!isPersonalRoom && <EndCallButton />}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default MeetingRoom
